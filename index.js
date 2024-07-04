6f775e395f1380a95582553a2714f9b77274915f
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-04 17:05:14 -0600

    2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+fetchCoordsByIP('70.64.52.116', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
